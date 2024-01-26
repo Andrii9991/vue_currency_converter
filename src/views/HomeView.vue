3a74a6aac2a41f74f2464431d54e18d862a5f237
Andrii9991 <117731577+Andrii9991@users.noreply.github.com>
2024-01-26 09:35:32 +0200

    create-project
    fix

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center text-bg-red-500"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class HomeView extends Vue {}
+</script>
